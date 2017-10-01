a1b509d8dcc05570bd1d8754a81ec7cfdd2bbce4
Joshua Vandaële <joshua@vandaele.software>
2017-10-01 14:08:46 +0200

    One entry!

--- Context Menu Editor.bat
+++ Context Menu Editor.bat
@@ -8,41 +8,70 @@ echo.Where is the icon file? (USE TWO \ EXEMPLE: C:\Windows.ico BECOME C:\\Windo
 set /p Icon=
 echo.Where is the executable? (USE TWO \ EXEMPLE: C:\Windows BECOME C:\\Windows.exe)
 set /p exec=
+echo.What do you want it to be? [1] a Menu (Open %exec% plus Open %exec% Elevated) [2] one entry (Open %exec%)
+choice /c 12
+IF %ERRORLEVEL% == 1 goto one
+IF %ERRORLEVEL% == 2 goto two
+exit
 
+:two
 echo.Windows Registry Editor Version 5.00 > Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\shell\01Menu%MUIVerb%] >> Menu.reg
-echo."MUIVerb"="%MUIVerb%" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo."ExtendedSubCommandsKey"="Directory\\ContextMenus\\Menu%MUIVerb%" >> Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\background\shell\01Menu%MUIVerb%] >> Menu.reg
-echo."MUIVerb"="%MUIVerb%" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo."ExtendedSubCommandsKey"="Directory\\ContextMenus\\Menu%MUIVerb%" >> Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\ContextMenus\Menu%MUIVerb%\shell\%MUIVerb%_admin] >> Menu.reg
-echo."MUIVerb"="%MUIVerb% Elevated" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb% Elevated" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo.@="powershell.exe -Command Start-Process %exec% -Verb RunAs" >> Menu.reg
 echo."HasLUAShield"="" >> Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\ContextMenus\Menu%MUIVerb%\shell\%MUIVerb%] >> Menu.reg
-echo."MUIVerb"="%MUIVerb%" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo.@="%exec%" >> Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\ContextMenus\Menu%MUIVerb%\shell\%MUIVerb%_admin\command] >> Menu.reg
-echo."MUIVerb"="%MUIVerb% Elevated" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb% Elevated" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo.@="powershell.exe -Command Start-Process %exec% -Verb RunAs" >> Menu.reg
 echo."HasLUAShield"="" >> Menu.reg
 echo. >> Menu.reg
 echo.[HKEY_CLASSES_ROOT\Directory\ContextMenus\Menu%MUIVerb%\shell\%MUIVerb%\command] >> Menu.reg
-echo."MUIVerb"="%MUIVerb%" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
 echo."Icon"="%Icon%" >> Menu.reg
 echo.@="%exec%" >> Menu.reg
+goto done
 
+:one
+echo.Windows Registry Editor Version 5.00 > Menu.reg
+echo.  >> Menu.reg
+echo.[HKEY_CLASSES_ROOT\Directory\shell\%MUIVerb%]  >> Menu.reg
+echo.@="" >> Menu.reg
+echo."Extended"="" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
+echo."Icon"="%Icon%" >> Menu.reg
+echo. >> Menu.reg
+echo.[HKEY_CLASSES_ROOT\Directory\shell\%MUIVerb%\command] >> Menu.reg 
+echo.@="%exec%" >> Menu.reg
+echo. >> Menu.reg
+echo.[HKEY_CLASSES_ROOT\Directory\Background\shell\%MUIVerb%] >> Menu.reg 
+echo.@="" >> Menu.reg
+echo."MUIVerb"="Open %MUIVerb%" >> Menu.reg
+echo."Icon"="%Icon%" >> Menu.reg
+echo. >> Menu.reg
+echo.[HKEY_CLASSES_ROOT\Directory\Background\shell\%MUIVerb%\command] >> Menu.reg 
+echo.@="%exec%" >> Menu.reg
+goto done
+
+:done
 echo Done!
 echo Open the file "menu.reg" to apply changes!
 echo if you wanna remove the context menu either use the given file or do it manually
